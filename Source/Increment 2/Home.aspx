ca8d334f2cde93a3b27327ba5786a1d3d0c1e256
Avinash2205 <avinashbanala.2205@gmail.com>
2017-10-13 20:28:12 -0500

    Increment 2
    Home and View Income Page

---
+++ Source/Increment 2/Home.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/MasterPage.master" CodeFile="Home.aspx.cs" Inherits="Home" %>
+
+<asp:Content ID="body" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+  
+    <div>
+    <h1>Hello !!! Welcome to Daily Expense Tracker</h1>
+    </div>
+ </asp:Content>
---
+++ Source/Increment 2/ViewInc.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class ViewInc : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=F:\Daily Expense Tracker (hemakshi naik)\Daily Expense Tracker (hemakshi naik)\Project\App_Data\Database.mdf;Integrated Security=True;User Instance=True");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        LabelUid.Text = Session["uid"].ToString();
+
+        SqlDataAdapter da = new SqlDataAdapter("Select * from inc where uid='" + LabelUid.Text + "'", con);
+          DataSet ds = new DataSet();
+          da.Fill(ds);
+
+          GridView1.DataSource = ds;
+          GridView1.DataBind();
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+        SqlDataAdapter da = new SqlDataAdapter("Select * from inc where uid='" + LabelUid.Text + "' and mon='" + DropDownList1.Text + "' and yr='" + TextBox2.Text + "'", con);
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+
+        GridView1.DataSource = ds;
+        GridView1.DataBind();
+    }
+}
